ae43f39c89462f3642cfdf09ed42eabab7bdfc10
Adel Labadi <blade.mimiga@hotmail.com>
2020-10-03 15:28:32 -0500

    Work in progress. Scrapes data from various sites to get status's and notifications from various content creators

---
+++ Notifier/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace Notifier
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.clintBox = new System.Windows.Forms.CheckBox();
+            this.daphBox = new System.Windows.Forms.CheckBox();
+            this.mang0Box = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.zainBox = new System.Windows.Forms.CheckBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.groupBox1.Controls.Add(this.zainBox);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.clintBox);
+            this.groupBox1.Controls.Add(this.daphBox);
+            this.groupBox1.Controls.Add(this.mang0Box);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.groupBox1.Location = new System.Drawing.Point(2, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(248, 352);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Active Streamers";
+            // 
+            // clintBox
+            // 
+            this.clintBox.AutoSize = true;
+            this.clintBox.BackColor = System.Drawing.Color.DarkRed;
+            this.clintBox.Enabled = false;
+            this.clintBox.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.clintBox.FlatAppearance.CheckedBackColor = System.Drawing.Color.Lime;
+            this.clintBox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.clintBox.ForeColor = System.Drawing.Color.Black;
+            this.clintBox.Location = new System.Drawing.Point(133, 48);
+            this.clintBox.Name = "clintBox";
+            this.clintBox.Size = new System.Drawing.Size(12, 11);
+            this.clintBox.TabIndex = 7;
+            this.clintBox.UseVisualStyleBackColor = false;
+            // 
+            // daphBox
+            // 
+            this.daphBox.AutoSize = true;
+            this.daphBox.BackColor = System.Drawing.Color.DarkRed;
+            this.daphBox.Enabled = false;
+            this.daphBox.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.daphBox.FlatAppearance.CheckedBackColor = System.Drawing.Color.Lime;
+            this.daphBox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.daphBox.ForeColor = System.Drawing.Color.Black;
+            this.daphBox.Location = new System.Drawing.Point(91, 126);
+            this.daphBox.Name = "daphBox";
+            this.daphBox.Size = new System.Drawing.Size(12, 11);
+            this.daphBox.TabIndex = 6;
+            this.daphBox.UseVisualStyleBackColor = false;
+            // 
+            // mang0Box
+            // 
+            this.mang0Box.AutoSize = true;
+            this.mang0Box.BackColor = System.Drawing.Color.DarkRed;
+            this.mang0Box.Enabled = false;
+            this.mang0Box.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.mang0Box.FlatAppearance.CheckedBackColor = System.Drawing.Color.Lime;
+            this.mang0Box.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.mang0Box.ForeColor = System.Drawing.Color.Black;
+            this.mang0Box.Location = new System.Drawing.Point(84, 84);
+            this.mang0Box.Name = "mang0Box";
+            this.mang0Box.Size = new System.Drawing.Size(12, 11);
+            this.mang0Box.TabIndex = 5;
+            this.mang0Box.UseVisualStyleBackColor = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 18);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "39daph";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Mang0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ClintStevens";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(168, 323);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(74, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Refresh";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 159);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 18);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Zainssbm";
+            // 
+            // zainBox
+            // 
+            this.zainBox.AutoSize = true;
+            this.zainBox.BackColor = System.Drawing.Color.DarkRed;
+            this.zainBox.Enabled = false;
+            this.zainBox.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.zainBox.FlatAppearance.CheckedBackColor = System.Drawing.Color.Lime;
+            this.zainBox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.zainBox.ForeColor = System.Drawing.Color.Black;
+            this.zainBox.Location = new System.Drawing.Point(107, 163);
+            this.zainBox.Name = "zainBox";
+            this.zainBox.Size = new System.Drawing.Size(12, 11);
+            this.zainBox.TabIndex = 9;
+            this.zainBox.UseVisualStyleBackColor = false;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(389, 34);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Size = new System.Drawing.Size(385, 380);
+            this.webBrowser1.TabIndex = 1;
+            this.webBrowser1.Url = new System.Uri("https://www.twitch.tv/mang0", System.UriKind.Absolute);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Notify";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox daphBox;
+        private System.Windows.Forms.CheckBox mang0Box;
+        private System.Windows.Forms.CheckBox clintBox;
+        private System.Windows.Forms.CheckBox zainBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ Notifier/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Notifier
+{
+    public partial class Form1 : Form
+    {
+        public BackgroundWorker bgWorker = new BackgroundWorker();
+        public string mang0 = "https://www.twitch.tv/mang0";
+        public string clint = "https://www.twitch.tv/clintstevens";
+        public string daph = "https://www.twitch.tv/39daph";
+        public string zain = "https://www.twitch.tv/zainssbm";
+        public List<string> streamers = new List<string>();
+        public bool ClintActive = false;
+        public bool Mang0Active = false;
+        public bool daphActive = false;
+        public bool zainActive = false;
+        public bool found = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            streamers.Add(mang0);
+            streamers.Add(clint);
+            streamers.Add(daph);
+            streamers.Add(zain);
+            getWebPage();
+            getStatus();
+        }
+        public void getWebPage()
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;           
+            foreach (string streamer in streamers)
+            {            
+             
+                webBrowser1.Navigate(streamer);
+                webBrowser1.DocumentCompleted += WebBrowser1_DocumentCompleted;
+                if(found == true)
+                {
+                    switch (streamer)
+                    {
+                        case "https://www.twitch.tv/mang0":
+                            Mang0Active = true;
+                            break;
+                        case "https://www.twitch.tv/clintstevens":
+                            ClintActive = true;
+                            break;
+                        case "https://www.twitch.tv/39daph":
+                            daphActive = true;
+                            break;
+                        case "https://www.twitch.tv/zainssbm":
+                            zainActive = true;
+                            break;
+                    }
+                    found = false;
+                }            
+            }                                          
+        }
+
+        private void WebBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (webBrowser1.Document.Body.InnerText.Contains("zain"))
+            {
+                found = true;
+            }
+        }
+
+        public void getStatus()
+        {
+            mang0Box.BackColor = (Mang0Active ? Color.LightGreen : Color.OrangeRed);
+            clintBox.BackColor = (ClintActive ? Color.LightGreen : Color.OrangeRed);
+            daphBox.BackColor = (daphActive ? Color.LightGreen : Color.OrangeRed);
+            zainBox.BackColor = (zainActive ? Color.LightGreen : Color.OrangeRed);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
